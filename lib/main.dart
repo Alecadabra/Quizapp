5e88adcf2798fcfd50b0d5d57c557b41691ba50c
Alec PC <27219575+Alecadabra@users.noreply.github.com>
2021-08-01 20:26:07 +0800

    Using themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'dart:io' show Platform;
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My App'),
+      home: HomeScreen(),
+      theme: ThemeData(
+        primaryColor: Colors.cyan,
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.deepPurple, fontSize: 30),
+          headline1: TextStyle(
+            color: Colors.amber,
+            fontSize: 70,
+            fontStyle: FontStyle.italic,
+          ),
         ),
-        body: Center(
-          child: Icon(Icons.cake),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: Theme.of(context).textTheme.headline1,
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.plus_one),
-        ),
-        drawer: Drawer(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.plus_one),
       ),
     );
   }
